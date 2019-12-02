5bb82aede6efb30f1ca878118676a8575ad7d1c3
Megan McGee <meganpmcgee@gmail.com>
2019-12-02 19:53:01 +0000

    Access agency resource properties with dot notation. Logged a string with agency ours and and agency name to console.

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ let agencyResource = {
   "x": "-118.2467481",
   "y": "34.04230597",
   "cat1": "Social Services",
-  "cat2:" "Homeless Shelters and Services",
+  "cat2": "Homeless Shelters and Services",
   "org_name":	"Lamp Community - Frank Rice Access Center",
   "addrln1": "627 San Julian St.",	
   "addrln2": "",
@@ -49,6 +49,13 @@ let agencyResource = {
   "state": "CA",	
   "hours": "Monday through Friday, 8:00am to 4:00pm. ",
   "phones": "Administrative (213) 488-9559, Service/Intake (213) 488-0031",
-  "description": "The agency provides homeless support services and shelter for adults who have chronic mental illness and who are in Skid Row and downtown Los Angeles, including people who are undocumented or who have developmental disabilities or a dual diagnosis of mental illness and substance abuse." 
+  "description": "The agency provides homeless support services and shelter for adults who have chronic mental illness and who are in Skid Row and downtown Los Angeles, including people who are undocumented or who have developmental disabilities or a dual diagnosis of mental illness and substance abuse.",
   "groups": ["undocumented", "developmental disabilities", "dual diagnosis", "mental illness", "substance abuse"]
 }
+
+// Dot notation in javascript
+
+
+// Uses dot notation to get the property of the org_name key
+let organization = agencyResource.org_name;
+console.log(organization + " is open "+ agencyResource.hours );
